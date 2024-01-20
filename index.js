75864f954683539a99b8098b40b80535e383c041
Abheet seth <114290748+AbheetHacker4278@users.noreply.github.com>
2024-01-20 16:03:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ let quoteElement = document.querySelector('.quote');
 let authorElement = document.querySelector('.author');
 let clickElement = document.querySelector('.clk');
 let inputElement = document.querySelector('.inpt');
+let imgg = document.querySelector('.img');
+imgg.style.display = 'none';
 clickElement.addEventListener('click', async function utility(){
     let data = await fetch(`https://api.quotable.io/random?tags=${inputElement.value}`);
     let struct = await data.json();
+    quoteElement.innerHTML = '';
+    authorElement.innerHTML = '';
+    imgg.style.display = 'block';
     console.log(struct);
-    quoteElement.innerHTML = `${struct?.content}`;
-    authorElement.innerHTML = `-${struct?.author}`;
+    setTimeout(function(){
+        quoteElement.innerHTML = `${struct?.content}`;
+        authorElement.innerHTML = `-${struct?.author}`;
+        imgg.style.display = 'none';
+    } , 2000);
 });
 
